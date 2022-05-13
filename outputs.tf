e0032e6c5f0d3fb9d377b6e98616f77da272e5bf
rahulnandan <rahul.nandan2007@gmail.com>
2022-05-13 13:46:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "elastic_ips" {
   description = "Provisioned Elastic IPs"
   value       = module.this.enabled && var.eip_enabled ? aws_eip.sftp.*.id : null
 }
+  
+output "transfer_id" {
+  description = "The id of the transfer server"
+  value       = module.this.enabled ? aws_transfer_server.default.*.id : null
+}
