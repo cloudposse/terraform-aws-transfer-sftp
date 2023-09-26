eb0d6627b55a586b213c245ff943d9b12b437121
paintoxic <andres.lopez@y.uno>
2023-09-26 10:24:26 -0500

    feat : add outputs of private and public ips

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,16 @@ output "elastic_ips" {
   value       = module.this.enabled && var.eip_enabled ? aws_eip.sftp.*.id : null
 }
 
+output "private_ips" {
+  description = "Provisioned Private IPs"
+  value       = module.this.enabled && var.eip_enabled ? aws_eip.sftp.*.private_ip : null
+}
+
+output "public_ips" {
+  description = "Provisioned Public IPs"
+  value       = module.this.enabled && var.eip_enabled ? aws_eip.sftp.*.public_ip : null
+}
+
 output "s3_access_role_arns" {
   description = "Role ARNs for the S3 access"
   value       = { for user, val in aws_iam_role.s3_access_for_sftp_users : user => val.arn }
